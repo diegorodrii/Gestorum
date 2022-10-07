972d157697b0c4640e23de146fa8a091a5d4a4fc
diegorodrii <diegomiragenil@gmail.com>
2022-10-07 11:15:42 +0200

    Feature

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,30 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
   },
   {
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
+  },
+  
+
+  {
+    path: 'people',
+    loadChildren: () => import('./pages/people/people.module').then( m => m.PeoplePageModule)
+  },
+  {
+    path: 'tasks',
+    loadChildren: () => import('./pages/tasks/tasks.module').then( m => m.TasksPageModule)
+  },
+  {
+    path: 'assign',
+    loadChildren: () => import('./pages/assign/assign.module').then( m => m.AssignPageModule)
+  },
+  {
+    path: 'viewtasks',
+    loadChildren: () => import('./pages/viewtasks/viewtasks.module').then( m => m.ViewtasksPageModule)
   },
 ];
 
